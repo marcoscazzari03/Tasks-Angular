c09ab754cc505a1b6fe35125abbad64cd0655bc3
Marco Scazzari <marco.scazzari@lefonti.com>
2025-06-19 10:34:16 +0200

    Updates

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
+
+@NgModule({
+    declarations: [AppComponent, HeaderComponent],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, UserComponent, TasksComponent]
+})
+export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-header',
-    standalone: true,   //Angular 19+ questo è già settato in auto
+    standalone: false,
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
